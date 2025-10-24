fc1bfda2e6d76ac7495f77bc78f3f457d7c76ec7
Darius Nicolae Alain Tirla <darius.tirla.na@gmail.com>
2025-10-25 01:03:19 +0200

    Add project files.

---
+++ SanLuigiPizzeria.Startup/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SanLuigiPizzeria.Startup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mamma mia pizzeria!");
+        }
+    }
+}
